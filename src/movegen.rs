f9da452afd30545b0a0ff9e70c24396af86b94e5
Baptiste Loison <baptisteloison8400@gmail.com>
2025-10-19 20:09:41 +0200

    check killer legality during generation of legal moves

--- src/movegen.rs
+++ src/movegen.rs
@@ -1,41 +1,39 @@
-use crate::movegen::MoveGenState::{Remaining, Sorted, TTMove};
 use crate::search::see_for_sort;
 use crate::TranspositionTable;
-use chess::{Board, ChessMove, MoveGen, EMPTY};
+use chess::{BitBoard, Board, ChessMove, MoveGen};
 use smallvec::SmallVec;
 
-pub const MVV_LVA_TABLE: [[u8; 6]; 6] = [
-    // Victim = Pawn
-    [15, 14, 13, 12, 11, 10],
-    // Victim = Knight
-    [25, 24, 23, 22, 21, 20],
-    // Victim = Bishop
-    [35, 34, 33, 32, 31, 30],
-    // Victim = Rook
-    [45, 44, 43, 42, 41, 40],
-    // Victim = Queen
-    [55, 54, 53, 52, 51, 50],
-    // Victim = King (we never actually capture kings in legal move generation)
-    [0, 0, 0, 0, 0, 0],
-];
+type MoveList = SmallVec<[ChessMove; 64]>;
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-enum MoveGenState {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum MoveGenPhase {
     TTMove,
-    Sorted,
-    Remaining,
+    GoodCaptures,
+    GoodQuiet,
+    BadCaptures,
+    BadQuiet,
+    Done,
 }
 
 pub struct IncrementalMoveGen<'a> {
-    iterator: MoveGen,
-    state: MoveGenState,
     board: &'a Board,
     tt_move: Option<ChessMove>,
     killer_moves: [Option<ChessMove>; 2],
-    move_buff: SmallVec<[ChessMove; 64]>,
-    capt_idx: Option<usize>,
+    phase: MoveGenPhase,
+    has_legal_moves: bool,
     capture_gen_pending: bool,
-    killer_idx: usize,
+    captures_generated: bool,
+    quiet_generated: bool,
+    good_captures: MoveList,
+    bad_captures: MoveList,
+    killer_quiet: MoveList,
+    good_quiet: MoveList,
+    bad_quiet: MoveList,
+    good_capture_idx: usize,
+    bad_capture_idx: usize,
+    killer_quiet_idx: usize,
+    good_quiet_idx: usize,
+    bad_quiet_idx: usize,
 }
 
 impl<'a> IncrementalMoveGen<'a> {
@@ -44,6 +42,9 @@ impl<'a> IncrementalMoveGen<'a> {
         tt: &TranspositionTable,
         killer_moves: [Option<ChessMove>; 2],
     ) -> Self {
+        let probe_iter = MoveGen::new_legal(board);
+        let has_legal_moves = probe_iter.len() > 0;
+
         let zob = board.get_hash();
         let tt_move = tt
             .probe(zob)
@@ -51,16 +52,114 @@ impl<'a> IncrementalMoveGen<'a> {
             .filter(|mv| board.legal(*mv));
 
         Self {
-            iterator: MoveGen::new_legal(&board),
-            state: TTMove,
             board,
             tt_move,
             killer_moves,
-            move_buff: SmallVec::new(),
-            capt_idx: None,
+            phase: MoveGenPhase::TTMove,
+            has_legal_moves,
             capture_gen_pending: false,
-            killer_idx: 0,
+            captures_generated: false,
+            quiet_generated: false,
+            good_captures: MoveList::new(),
+            bad_captures: MoveList::new(),
+            killer_quiet: MoveList::new(),
+            good_quiet: MoveList::new(),
+            bad_quiet: MoveList::new(),
+            good_capture_idx: 0,
+            bad_capture_idx: 0,
+            killer_quiet_idx: 0,
+            good_quiet_idx: 0,
+            bad_quiet_idx: 0,
         }
+    }
+
+    fn ensure_captures(&mut self) {
+        if self.captures_generated {
+            return;
+        }
+        self.captures_generated = true;
+        self.capture_gen_pending = true;
+
+        let mut capture_mask = *self.board.color_combined(!self.board.side_to_move());
+        if let Some(ep_sq) = self.board.en_passant() {
+            capture_mask |= BitBoard::from_square(ep_sq);
+        }
+
+        let mut gen = MoveGen::new_legal(self.board);
+        gen.set_iterator_mask(capture_mask);
+
+        let mut good_scored: SmallVec<[(ChessMove, i32); 32]> = SmallVec::new();
+        let mut bad_scored: SmallVec<[(ChessMove, i32); 32]> = SmallVec::new();
+
+        for mv in gen {
+            if Some(mv) == self.tt_move {
+                continue;
+            }
+
+            let mut score = see_for_sort(self.board, mv);
+            if score >= 0 {
+                score += 1;
+                good_scored.push((mv, score));
+            } else {
+                bad_scored.push((mv, score));
+            }
+        }
+
+        good_scored.sort_unstable_by(|a, b| b.1.cmp(&a.1));
+        bad_scored.sort_unstable_by(|a, b| b.1.cmp(&a.1));
+
+        self.good_captures
+            .extend(good_scored.into_iter().map(|(mv, _)| mv));
+        self.bad_captures
+            .extend(bad_scored.into_iter().map(|(mv, _)| mv));
+    }
+
+    fn ensure_quiet(&mut self) {
+        if self.quiet_generated {
+            return;
+        }
+        self.quiet_generated = true;
+
+        let mut killer_hits: MoveList = MoveList::new();
+        let mut good_quiet: MoveList = MoveList::new();
+        let mut bad_quiet: MoveList = MoveList::new();
+        let mut killer_seen = [false; 2];
+
+        let gen = MoveGen::new_legal(self.board);
+        for mv in gen {
+            if Some(mv) == self.tt_move {
+                continue;
+            }
+            if self.board.piece_on(mv.get_dest()).is_some() {
+                continue;
+            }
+
+            let mut matched_killer = false;
+            for (idx, killer) in self.killer_moves.iter().enumerate() {
+                if killer_seen[idx] {
+                    continue;
+                }
+                if *killer == Some(mv) {
+                    killer_seen[idx] = true;
+                    killer_hits.push(mv);
+                    matched_killer = true;
+                    break;
+                }
+            }
+            if matched_killer {
+                continue;
+            }
+
+            if mv.get_promotion().is_some() {
+                good_quiet.push(mv);
+            } else {
+                bad_quiet.push(mv);
+            }
+        }
+
+        self.killer_quiet = killer_hits;
+        self.good_quiet = good_quiet;
+        self.bad_quiet = bad_quiet;
     }
 
     pub fn take_capture_generation_event(&mut self) -> bool {
@@ -74,7 +173,7 @@ impl<'a> IncrementalMoveGen<'a> {
 
     // Game's over if player cannot play a single move
     pub fn is_over(&self) -> bool {
-        self.iterator.len() == 0
+        !self.has_legal_moves
     }
 }
 
@@ -82,71 +181,57 @@ impl Iterator for IncrementalMoveGen<'_> {
     type Item = ChessMove;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.state == TTMove {
-            self.state = Sorted;
-            if let Some(tt_mv) = self.tt_move {
-                return Some(tt_mv);
-            }
-        }
-
-        if self.state == Sorted {
-            if self.capt_idx.is_none() {
-                // Sort for captures
-                let targets = self.board.color_combined(!self.board.side_to_move());
-                self.iterator.set_iterator_mask(*targets);
-
-                let mut scored_moves: SmallVec<[(ChessMove, i32); 32]> = SmallVec::new();
-                for mv in &mut self.iterator {
-                    if Some(mv) == self.tt_move {
-                        continue;
-                    }
-
-                    let mut score = see_for_sort(self.board, mv);
-                    if score >= 0 {
-                        score += 1;
-                    }
-                    scored_moves.push((mv, score));
-                }
-
-                while self.killer_idx < self.killer_moves.len() {
-                    let killer = self.killer_moves[self.killer_idx];
-                    self.killer_idx += 1;
-                    if let Some(kmv) = killer {
-                        if Some(kmv) == self.tt_move {
-                            continue;
-                        }
-                        if self.board.piece_on(kmv.get_dest()).is_some() {
-                            continue;
-                        }
-                        if self.board.legal(kmv) {
-                            scored_moves.push((kmv, 0));
-                        }
-                    }
-                }
-
-                scored_moves.sort_unstable_by(|a, b| b.1.cmp(&a.1));
-                self.move_buff
-                    .extend(scored_moves.into_iter().map(|(m, _)| m));
-                self.capt_idx = Some(0);
-                self.capture_gen_pending = true;
-            }
-
-            if (self.capt_idx.unwrap()) < self.move_buff.len() {
-                let mv = self.move_buff[self.capt_idx.unwrap()];
-                self.capt_idx = Some(self.capt_idx.unwrap() + 1);
-                return Some(mv);
-            } else {
-                self.state = Remaining;
-                self.iterator.set_iterator_mask(!EMPTY);
-            }
-        }
-
         loop {
-            let mv = self.iterator.next();
-            if self.tt_move.is_some() && mv == self.tt_move {
-                continue; // skip duplicates
+            match self.phase {
+                MoveGenPhase::TTMove => {
+                    self.phase = MoveGenPhase::GoodCaptures;
+                    if let Some(tt_mv) = self.tt_move {
+                        return Some(tt_mv);
+                    }
+                }
+                MoveGenPhase::GoodCaptures => {
+                    self.ensure_captures();
+                    if self.good_capture_idx < self.good_captures.len() {
+                        let mv = self.good_captures[self.good_capture_idx];
+                        self.good_capture_idx += 1;
+                        return Some(mv);
+                    }
+                    self.phase = MoveGenPhase::GoodQuiet;
+                }
+                MoveGenPhase::GoodQuiet => {
+                    self.ensure_quiet();
+                    if self.killer_quiet_idx < self.killer_quiet.len() {
+                        let mv = self.killer_quiet[self.killer_quiet_idx];
+                        self.killer_quiet_idx += 1;
+                        return Some(mv);
+                    }
+                    if self.good_quiet_idx < self.good_quiet.len() {
+                        let mv = self.good_quiet[self.good_quiet_idx];
+                        self.good_quiet_idx += 1;
+                        return Some(mv);
+                    }
+                    self.phase = MoveGenPhase::BadCaptures;
+                }
+                MoveGenPhase::BadCaptures => {
+                    self.ensure_captures();
+                    if self.bad_capture_idx < self.bad_captures.len() {
+                        let mv = self.bad_captures[self.bad_capture_idx];
+                        self.bad_capture_idx += 1;
+                        return Some(mv);
+                    }
+                    self.phase = MoveGenPhase::BadQuiet;
+                }
+                MoveGenPhase::BadQuiet => {
+                    self.ensure_quiet();
+                    if self.bad_quiet_idx < self.bad_quiet.len() {
+                        let mv = self.bad_quiet[self.bad_quiet_idx];
+                        self.bad_quiet_idx += 1;
+                        return Some(mv);
+                    }
+                    self.phase = MoveGenPhase::Done;
+                }
+                MoveGenPhase::Done => return None,
             }
-            return mv;
         }
     }
 }
