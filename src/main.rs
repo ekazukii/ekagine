3ad651e89f39de7e753241fd51bc75ff5320c604
Baptiste Loison <baptisteloison8400@gmail.com>
2026-02-01 16:34:04 +0100

    bump version

--- src/main.rs
+++ src/main.rs
@@ -236,7 +236,7 @@ fn uci_loop() {
 
         match tokens[0] {
             "uci" => {
-                send_message(&mut stdout, "id name Ekagine-v2.10.0");
+                send_message(&mut stdout, "id name Ekagine-v2.10.1");
                 send_message(&mut stdout, "id author BaptisteLoison");
                 send_message(
                     &mut stdout,
